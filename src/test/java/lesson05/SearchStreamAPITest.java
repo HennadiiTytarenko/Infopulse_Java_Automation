4e7e9cfdde4fefaae31134aac337e422dfac9ad2
Hennadii Tytarenko <Gena080393@gmail.com>
2019-09-02 20:44:33 +0300

    SearchStreamAPI test

---
+++ src/test/java/lesson05/SearchStreamAPITest.java
@@ -0,0 +1,63 @@
+package lesson05;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SearchStreamAPITest {
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+
+       //System.setProperty("webdriver.chrome.driver","D:\\HARD WORK\\Testing\\Automation\\Java\\Infopulse\\test-automation-5-master\\src\\driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+
+        driver.get("http://automationpractice.com/index.php");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+
+    @Test(timeout = 5000l)
+    public void verifyFirstTipIsCorrect() {
+        driver.findElement(By.id("search_query_top"))
+                .clear();
+        driver.findElement(By.id("search_query_top"))
+                .sendKeys("Summer Dress");
+
+        List<WebElement> results = driver.findElements(By.xpath("//div[@class = \"ac_results\"]//li"));
+        Stream<WebElement> resultsStream = results.stream();
+
+        Assert.assertTrue("not all matches", resultsStream.allMatch(s->s.getText().contains("Summer Dress")));
+
+
+           /* resultsStream
+                .map(s->s.getText())
+                .filter(s->!s.contains("Summer Dress"))
+                .forEach(b-> System.out.println(b));*/
+        }
+
+    }
+
+
+
